522a35d639db4a780a8931ed9274c11a60e8bff1
unknown <aakashsri.dev@gmail.com>
2021-11-03 16:32:17 +0530

    Showing default colors of organge instead of empty initially

--- 09-color-generator/setup/src/App.js
+++ 09-color-generator/setup/src/App.js
@@ -6,14 +6,7 @@ import Values from "values.js";
 function App() {
   const [input, setInput] = useState("");
   const [error, setError] = useState(false);
-  const [list, setList] = useState([]);
-
-  // function componentToHex(c) {
-  //   const r = parseInt(c.slice(1,3),16)
-  //   const g = parseInt(c.slice(3,5),16)
-  //   const b = parseInt(c.slice(5,7),16)
-  //   return `${r},${g},${b}`
-  // }
+  const [list, setList] = useState(new Values('#f14053').all(10));
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -27,9 +20,7 @@ function App() {
       console.log(error);
     }
   };
-  // get the hex value;
-  // covert into rgb
-  // loop and add different values to rgb
+
   return (
     <>
       <section className="container">
