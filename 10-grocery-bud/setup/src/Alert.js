8fdba76c6c5e97460e6994ea1eeaad0b8d3dbf2e
unknown <aakashsri.dev@gmail.com>
2021-11-07 20:43:48 +0530

    Showing alert message dynamically Remove multiple if statements to check type Appended the 'type' directly inside className

--- 10-grocery-bud/setup/src/Alert.js
+++ 10-grocery-bud/setup/src/Alert.js
@@ -1,19 +1,13 @@
 import React, { useEffect } from "react";
 
-const Alert = ({ type, msg, setAlert ,items}) => {
+const Alert = ({ type, msg, setAlert, items }) => {
   useEffect(() => {
     let showToaster = setTimeout(() => {
       setAlert({ ...alert, show: false });
     }, 3000);
     return () => clearTimeout(showToaster);
-  },[items]);
-  if (type === "success") {
-    return <p className="alert alert-success">{msg}</p>;
-  }
-
-  if (type === "danger") {
-    return <p className="alert alert-danger">{msg}</p>;
-  }
+  }, [items]);
+  return <p className={`alert alert-${type}`}>{msg}</p>;
 };
 
 export default Alert;
